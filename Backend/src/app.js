043c808e5def52f2c471fa295c0b90fd59178227
Sanyam <shanujindal48@gmail.com>
2022-04-03 16:35:20 +0530

    barebones backed API's completed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,8 @@ const app = express()
 const db = require('./db')
 const port = 3000
 
+app.use(express.json())
+
 // Get all movies in the database
 app.get('/movies', (req, res) => {
     db.query("SELECT * FROM movie", (err, result) => {
@@ -30,11 +32,32 @@ app.get('/movies/:id', (req, res) => {
 
 // Get seats/tickets for a particular show for a movie
 app.get('/movies/:id/:showId', (req, res) => {
-    db.query(`SELECT * FROM tickets WHERE showId=${req.params.showId}`, (err, tickets) => {
+    db.query(`SELECT * FROM tickets WHERE showId=${req.params.showId} AND availability=1`, (err, tickets) => {
         if(err)
             return res.send(err)
         
         res.send(tickets)
+    })
+})
+
+// Book a ticket and block it for 1 minute
+app.post('/movies/:id/:showId/', (req, res) => {
+    const ticket = req.body['ticket']
+    db.query(`UPDATE tickets SET availability=0 WHERE showId=${req.params.showId} AND seat_number='${ticket}'`, (err, result) => {
+        if(err)
+            return res.send(err)
+        
+        res.send({"success": 'Ticket booked successfully!'})
+    })
+})
+
+// Show booked tickets
+app.get('/movies/:id/:showId/booked', (req, res) => {
+    db.query(`SELECT * FROM tickets WHERE availability=0`, (err, result) => {
+        if(err)
+            return res.send(err)
+        
+        res.send(result)
     })
 })
 
